860692b0b47816db3b262b918274e54a9945f4b8
Muskan Anjum <YOUR_EMAIL@gmail.com>
2026-09-24 11:50:10 +0530

    Secure FastAPI CORS configuration

--- fastapi_app.py
+++ fastapi_app.py
@@ -20,10 +20,14 @@ api = FastAPI(
 
 api.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://employee-activity-tracker.onrender.com",
+        "http://127.0.0.1:5000",
+        "http://localhost:5000",
+    ],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST"],
+    allow_headers=["Content-Type", "Authorization"],
 )
 
 
